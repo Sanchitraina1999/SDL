450248364ac926c1745e4e43be5ecf9b8fde22be
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-08-15 18:41:07 +0530

    stack

---
+++ rest/exampleStack.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class exampleStack {
+    public static void main(String[] args){
+        Stack s = new Stack();
+        for(int i = 0; i<10;i++){
+            s.push(i);
+        }
+        System.out.println(s.peek());
+        s.pop();
+        System.out.println(s);
+        int i=2;
+        if(s.search(i)==-1){
+            System.out.println("Not Found");
+        }
+        else{
+            System.out.println("Found "+ i);
+        }
+    }
+}
