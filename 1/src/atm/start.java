b51920a56cb2880cbe36744b53c62d76647df5fe
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-08-17 16:23:07 +0530

    remove stuff

--- 1/src/atm/start.java
+++
@@ -1,9 +0,0 @@
-import atm.*;
-import java.io.IOException;
-
- class start {
-    public static void main(String[] args){
-        optionMenu menu = new optionMenu();
-        menu.getLoginDetails();
-    }
-}
