c04d2f5e44d13c1fc10ad70665c64cd984f8b767
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-12-05 16:20:08 +0530

    update: master

--- 31201_sdl_04/AdmissionPortalGUI/src/connection/ConnectionClass.java
+++ 31201_sdl_04/AdmissionPortalGUI/src/connection/ConnectionClass.java
@@ -4,24 +4,21 @@ import java.sql.*;
 
 public class ConnectionClass {
 	public Connection con;
-	
-	public ConnectionClass()
-	{
+
+	public ConnectionClass() {
 		try {
 			String url = "jdbc:mysql://localhost:3306/collegeAdmissionsPortal";
 			String user = "root";
 			String password = "Abhi.20july";
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection(url, user, password);
-//			System.out.println("Connected to Database");
-			
+			// System.out.println("Connected to Database");
 		} catch (Exception e) {
 			System.out.println("Could Not Connect to Database");
 		}
 	}
-	
-	public Connection returnConnection()
-	{
+
+	public Connection returnConnection() {
 		return con;
 	}
 }
